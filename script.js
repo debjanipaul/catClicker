59c2397fc0c42e224dcdc301a3099f77009d0b97
Debjani Paul <debjanideb@hotmail.com>
2019-02-22 23:29:18 -0800

    added few more cats and events

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
-let catPhoto = document.getElementById('catPic');
-let catPhoto1= document.getElementById('catPic1');
-let clicks = document.getElementById('clicks');
-let clicks1 = document.getElementById('clicks1');
-let name1 = document.getElementById('name1');
-let name2 = document.getElementById('name2');
-let score = 0;
-let score1 = 0;
 
-catPhoto.addEventListener("click", function() {
-  score++;
-  clicks.innerHTML = score;
-  name2.innerHTML = 'Toby';
-})
+let cats =  ['Ruby', 'Toby', 'Jinny', 'Max', 'Jack'];
+let images = ['images/catPic.jpg', 'images/catPic1.jpg', 'images/catPic2.jpg', 'images/catPic3.jpg',
+                'images/catPic4.jpg'];
 
-catPhoto1.addEventListener("click", function() {
-  score1++;
-  clicks1.innerHTML = score1;
-    name1.innerHTML = 'Ruby';
-    console.log(clicks1.innerHTML);
-})
+
+
+for(var i = 0; i < cats.length; i++){
+
+  let catList = document.getElementById('listOfCats');
+  let name = document.getElementById('name');
+  let catPics = document.getElementById('catPics');
+  let clicks = document.getElementById('clicks1');
+  let score = 0;
+  let cat, image;
+
+  cat = cats[i];
+  image = images[i];
+
+  //create cat catList
+  let catNameList = document.createElement('li');
+  catNameList.innerHTML = cat;
+  catList.appendChild(catNameList);
+  catNameList.classList.add('clickCat');
+
+  //Display cat images
+  catNameList.addEventListener("click",(function(e) {
+      return function(e) {
+        catPics.innerHTML = '';
+        name.innerHTML = '';
+        clicks.innerHTML = '';
+
+        //Display cat name
+        let catName = document.createElement('h6');
+        catName.innerHTML = cat;
+        catName.classList.add('catName');
+        name.appendChild(catName);
+
+        //Display cat images
+        let catImage = document.createElement('img');
+        catImage.src = image;
+        catImage.classList.add('pic');
+        catPics.appendChild(catImage);
+
+        //counter functionality
+        catImage.addEventListener("click", (function(e) {
+          return function(e) {
+            score++;
+            clicks.innerHTML = score;
+          }
+        })(cat));
+
+      }
+  })(cat));
+
+
+};
